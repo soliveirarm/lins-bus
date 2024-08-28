bc5aa44f80addf608384e62f4044be6ce3fcf989
Sarah Oliveira <soliveirarm@outlook.com>
2024-08-28 02:16:53 -0300

    fix (LinesModal): fix table and desktop menus layout

--- src/components/LinesModal.jsx
+++ src/components/LinesModal.jsx
@@ -7,14 +7,19 @@ function LinesModal({ itinerario, line, setLine, isHidden, setIsHidden }) {
     setLine(line)
     hideLineMenu()
   }
+  const smallClasses =
+    "sm:h-auto sm:w-1/2 sm:top-1/2 sm:left-1/2 sm:-translate-x-1/2 sm:-translate-y-1/2"
   return (
     <div
       className={`${
         isHidden ? "hidden" : null
-      } h-full overflow-y-scroll fixed top-0 left-[10%] w-full`}
+      } h-full w-screen fixed top-0 left-[10%] overflow-y-scroll ${smallClasses} rounded`}
     >
       <div className="relative bg-black">
-        <button onClick={hideLineMenu} className="absolute right-12 pt-5">
+        <button
+          onClick={hideLineMenu}
+          className="absolute right-12 sm:right-6 pt-5"
+        >
           <IoIosClose size="36" />
         </button>
         <div className="flex flex-col p-4 pt-20 gap-2">
@@ -23,7 +28,7 @@ function LinesModal({ itinerario, line, setLine, isHidden, setIsHidden }) {
               onClick={() => handleClick(item.linha)}
               className={`${
                 line === item.linha ? "selected" : null
-              } line-button text-xl font-semibold py-3 px-4`}
+              } line-button text-xl font-semibold py-3 px-4 cursor-pointer rounded`}
               key={crypto.randomUUID()}
             >
               {item.linha} - {item.nome}
